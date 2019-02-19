48631df16e892898dbecac88c980efa38b2a7468
trondr <trondr@outlook.com>
2019-02-19 20:29:13 +0100

    Start implementing sdpeval

---
+++ src/lib/sdpeval/sdp.fs
@@ -0,0 +1,14 @@
+﻿namespace sdpeval
+
+module sdp =
+    
+    open sdpeval.charp
+    open System.Xml.Serialization
+    open System.IO
+    open Microsoft.UpdateServices.Administration
+
+    let loadsdp (sdpXmlFile:string) =         
+        let serializer = new XmlSerializer(typeof<SoftwareDistributionPackage>)
+        use sr = new StreamReader(sdpXmlFile)
+        let softwareDistributionPackage = serializer.Deserialize(sr) :?> SoftwareDistributionPackage
+        softwareDistributionPackage        
---
+++ src/test/spdeval.tests/sdptests.fs
@@ -0,0 +1,12 @@
+﻿namespace spdeval.tests
+
+
+module sdptest =
+    open NUnit.Framework
+    open sdpeval.sdp
+
+    [<Test>]
+    [<TestCase("C:\\Temp\\DriverToolCache\\HpCatalogForSms.latest\\\V2\\00004850-0000-0000-5350-000000065821.sdp")>]
+    let loadsdpTest (fileName) =
+        let actual = loadsdp fileName
+        ()
