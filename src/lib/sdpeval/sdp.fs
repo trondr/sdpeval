32a6ac3c6c5f030212a58b400a3a334ee5a32e8e
trondr <trondr@outlook.com>
2019-02-19 23:32:16 +0100

    Implement logical operators: And, Or, Not, True, False

--- src/lib/sdpeval/sdp.fs
+++ src/lib/sdpeval/sdp.fs
@@ -12,4 +12,64 @@ module sdp =
         let softwareDistributionPackage = new SoftwareDistributionPackage(sdpXmlFile)
         softwareDistributionPackage        
 
+    type WmiQuery = {NameSpace:string;WqlQuery:string}
+
+    type ApplicabilityRule =
+        |True
+        |False
+        |And of ApplicabilityRule seq
+        |Or of ApplicabilityRule seq
+        |Not of ApplicabilityRule
+        |WmiQuery of WmiQuery
+
+    open System
+    open System.Management
+    
+    let wmiQueryIsMatch (nameSpace:string) (wqlQuery:string) :bool = 
+            let managementPath = new ManagementPath(nameSpace)
+            let scope = new ManagementScope(managementPath)            
+            let query = new ObjectQuery(wqlQuery)
+            use searcher = new ManagementObjectSearcher(scope,query)
+            use managementObjectCollection = searcher.Get()
+            if(managementObjectCollection.Count = 0) then                
+                false
+            else
+                true
+
+    open System.Xml.Linq
+    open System.Xml
+
+    let rec sdpXmlToApplicabilityRules (applicabilityXml:string) =
+        let nameTable = new NameTable()
+        let namespaceManager = new XmlNamespaceManager(nameTable);
+        namespaceManager.AddNamespace("lar","http://schemas.microsoft.com/wsus/2005/04/CorporatePublishing/LogicalApplicabilityRules.xsd")
+        namespaceManager.AddNamespace("bar","http://schemas.microsoft.com/wsus/2005/04/CorporatePublishing/BaseApplicabilityRules.xsd")
+        let xmlParserContext = XmlParserContext(null,namespaceManager,null,XmlSpace.None)
+        use xmlReader = new XmlTextReader(applicabilityXml,XmlNodeType.Element,xmlParserContext)
+        let xElement = XElement.Load(xmlReader)
+        match xElement.Name.LocalName with
+        |"True" -> True
+        |"False" -> False
+        |"WmiQuery" -> WmiQuery{NameSpace="";WqlQuery=""}
+        |"And" -> 
+            And (xElement.Descendants()
+            |>Seq.map (fun x -> (sdpXmlToApplicabilityRules (x.ToString()))))
+        |"Or" -> 
+            Or (xElement.Descendants()
+            |>Seq.map (fun x -> (sdpXmlToApplicabilityRules (x.ToString()))))
+        |"Not" -> 
+            Not (sdpXmlToApplicabilityRules (xElement.FirstNode.ToString()))
+        |_ -> False
+
+    let rec evaluateApplicabilityRule applicabilityRule =
+        match applicabilityRule with
+        |True -> true
+        |False -> false
+        |And al -> 
+            al |> Seq.forall evaluateApplicabilityRule
+        |Or al -> 
+            al |> Seq.exists evaluateApplicabilityRule
+        |Not al -> 
+            not (evaluateApplicabilityRule al)
+        |WmiQuery wq -> (wmiQueryIsMatch wq.NameSpace wq.WqlQuery)
     
--- src/test/spdeval.tests/sdptests.fs
+++ src/test/spdeval.tests/sdptests.fs
@@ -12,3 +12,21 @@ module sdptest =
         Assert.IsNotNull(actual.Description)
         printf "%s" actual.IsInstallable
         ()
+    
+    [<Test>]
+    [<TestCase("<lar:And><lar:True /><lar:True /></lar:And>",true)>]
+    [<TestCase("<lar:And><lar:False /><lar:True /></lar:And>",false)>]
+    [<TestCase("<lar:And><lar:True /><lar:False /></lar:And>",false)>]
+    [<TestCase("<lar:And><lar:False /><lar:False /></lar:And>",false)>]
+
+    [<TestCase("<lar:Or><lar:True /><lar:True /></lar:Or>",true)>]
+    [<TestCase("<lar:Or><lar:False /><lar:True /></lar:Or>",true)>]
+    [<TestCase("<lar:Or><lar:True /><lar:False /></lar:Or>",true)>]
+    [<TestCase("<lar:Or><lar:False /><lar:False /></lar:Or>",false)>]
+
+    [<TestCase("<lar:Not><lar:False /></lar:Not>",true)>]
+    [<TestCase("<lar:Not><lar:True /></lar:Not>",false)>]
+    let sdpXmlToApplicabilityRulesTests (xmlString,expectedEvaluation:bool) =
+        let actual = sdpXmlToApplicabilityRules xmlString
+        let actualEvaluation = evaluateApplicabilityRule actual
+        Assert.AreEqual(expectedEvaluation,actualEvaluation)
