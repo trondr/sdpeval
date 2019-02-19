9b4c87e91b5256abf02758c21bb7de0aee8a1ad2
trondr <trondr@outlook.com>
2019-02-19 21:17:28 +0100

    Use Microsoft.UpdateServices.Administration namespace to load sdp xml file with software distribution package definition.

--- src/test/spdeval.tests/sdptests.fs
+++ src/test/spdeval.tests/sdptests.fs
@@ -9,4 +9,6 @@ module sdptest =
     [<TestCase("C:\\Temp\\DriverToolCache\\HpCatalogForSms.latest\\\V2\\00004850-0000-0000-5350-000000065821.sdp")>]
     let loadsdpTest (fileName) =
         let actual = loadsdp fileName
+        Assert.IsNotNull(actual.Description)
+        printf "%s" actual.IsInstallable
         ()
