656aecf25aa24c1762de35a23572699a965a8739
trondr <trondr@outlook.com>
2019-04-01 20:17:43 +0200

    Update RegSz tests with (hopefully) a registry key that exists on all models. Add a Dell specific Wmi Query test.

--- src/test/spdeval.tests/RegSzTests.fs
+++ src/test/spdeval.tests/RegSzTests.fs
@@ -11,10 +11,10 @@ module RegSzTests =
 
     let internal testData = 
         [
-            yield {RegSz={Comparison="EqualTo";Key="HKEY_LOCAL_MACHINE";Subkey="SOFTWARE\Classes\AppID\{00944ad3-b2ad-4bcf-9202-59bf4662d521}";RegType32=Some "True";Value="RunAs";Data="Interactive User"};Expected=true}
-            yield {RegSz={Comparison="EqualTo";Key="HKEY_LOCAL_MACHINE";Subkey="SOFTWARE\Classes\AppID\{00944ad3-b2ad-4bcf-9202-59bf4662d521}";RegType32=Some "False";Value="RunAs";Data="Interactive User"};Expected=true}
-            yield {RegSz={Comparison="EqualTo";Key="HKEY_LOCAL_MACHINE";Subkey="SOFTWARE\Classes\AppID\{00944ad3-b2ad-4bcf-9202-59bf4662d521}";RegType32=Some "True";Value="";Data="Local Service Credential UI Broker"};Expected=true}
-            yield {RegSz={Comparison="EqualTo";Key="HKEY_LOCAL_MACHINE";Subkey="SOFTWARE\Classes\AppID\{00944ad3-b2ad-4bcf-9202-59bf4662d521}";RegType32=Some "False";Value="";Data="Local Service Credential UI Broker"};Expected=true}
+            yield {RegSz={Comparison="EqualTo";Key="HKEY_LOCAL_MACHINE";Subkey="SOFTWARE\Classes\AppID\{1202DB60-1DAC-42C5-AED5-1ABDD432248E}";RegType32=Some "True";Value="RunAs";Data="Interactive User"};Expected=true}
+            yield {RegSz={Comparison="EqualTo";Key="HKEY_LOCAL_MACHINE";Subkey="SOFTWARE\Classes\AppID\{1202DB60-1DAC-42C5-AED5-1ABDD432248E}";RegType32=Some "False";Value="RunAs";Data="Interactive User"};Expected=true}
+            yield {RegSz={Comparison="EqualTo";Key="HKEY_LOCAL_MACHINE";Subkey="SOFTWARE\Classes\AppID\{09C5C2B5-1D32-4598-B87E-203F32BB08E3}";RegType32=Some "True";Value="";Data="Windows Media Player Rich Preview Handler"};Expected=true}
+            yield {RegSz={Comparison="EqualTo";Key="HKEY_LOCAL_MACHINE";Subkey="SOFTWARE\Classes\AppID\{09C5C2B5-1D32-4598-B87E-203F32BB08E3}";RegType32=Some "False";Value="";Data="Windows Media Player Rich Preview Handler"};Expected=true}
             yield {RegSz={Comparison="EqualTo";Key="HKEY_LOCAL_MACHINE";Subkey="SOFTWARE\Microsoft\Windows\CurrentVersion";RegType32=Some "False";Value="ProgramFilesDir";Data="C:\Program Files"};Expected=true}
             yield {RegSz={Comparison="EqualTo";Key="HKEY_LOCAL_MACHINE";Subkey="SOFTWARE\Microsoft\Windows\CurrentVersion";RegType32=Some "True";Value="ProgramFilesDir";Data="C:\Program Files (x86)"};Expected=true}
             yield {RegSz={Comparison="EqualTo";Key="HKEY_CURRENT_USER";Subkey="Software\Microsoft\Windows\CurrentVersion\ThemeManager";RegType32=Some "False";Value="ColorName";Data="NormalColor"};Expected=true}
