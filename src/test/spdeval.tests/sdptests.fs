48d8a2418ac3939cd8c76030627ece15a82cc0e3
trondr <trondr@outlook.com>
2019-10-10 22:16:05 +0200

    Fixed issue with failing test due to running on un supported hardware.

--- src/test/spdeval.tests/sdptests.fs
+++ src/test/spdeval.tests/sdptests.fs
@@ -126,7 +126,7 @@ module sdptest =
     [<TestCase("25.Processor:Architecture 9","854A","<lar:And><bar:Processor Architecture=\"9\" Level=\"6\" Revision=\"36364\"/></lar:And>",true,"This unit test might fail depending on the current processor revision. Run successfully on HP 830 G6 (854A).",false)>]
     [<TestCase("26.WmiQuery: Complex Logic","SomeDell","<lar:And><bar:WmiQuery Namespace=\"Root\\Dell\\sysinv\" WqlQuery=\"SELECT * FROM Dell_SoftwareIdentity WHERE (Description &gt;= 'Dell:APAC_102511__' AND Description &lt; 'Dell:APAC_102511__~')\" /><lar:Not><bar:WmiQuery Namespace=\"Root\Dell\sysinv\" WqlQuery=\"SELECT * FROM Dell_SoftwareIdentity WHERE (Description &gt;= 'Dell:APAC_102511__' AND Description &lt; 'Dell:APAC_102511__~') AND VersionString &lt; '0001.0002.1004.0000'\" /></lar:Not></lar:And>",false,"This unit test might fail depending on the current PC model not beeing a Dell and that Dell sysinventory agent not beeing installed.",false)>]
     [<TestCase("27.WmiQuery: Complex Logic->True","AllModels","<lar:And>
-    <bar:WmiQuery Namespace=\"Root\cimv2\" WqlQuery=\"select * from Win32_ComputerSystem where (Manufacturer='Hewlett-Packard' and not (Model like '%Proliant%')) or (Manufacturer='HP') \" />
+    <bar:WmiQuery Namespace=\"Root\cimv2\" WqlQuery=\"select * from Win32_ComputerSystem where (Manufacturer='Hewlett-Packard' and not (Model like '%Proliant%')) or (Manufacturer='HP') or (Manufacturer='System manufacturer') \" />
     <lar:Or>
       <lar:And>
         <bar:Processor Architecture=\"9\" />
